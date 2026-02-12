702f1d939170e36a879ddfbedc7170524fe2a215
Priyanshu Chaudhary <priyanshuchaudhary53@gmail.com>
2026-02-13 03:46:24 +0530

    fix: update styles for project list image and icon colors

--- src/components/project/projectList.js
+++ src/components/project/projectList.js
@@ -9,13 +9,13 @@ export default function ProjectList({className}) {
                     <li key={project.id} className="line-y">
                         <a href={project.url} target="_blank" rel="noopener noreferrer" className="flex items-center gap-4 px-4 py-2 sm:px-2 transition-colors hover:bg-gray-950/2.5 ">
                             <div className="flex grow items-center gap-4">
-                                <Image src={project.image} alt={`${project.title} Logo`} width={48} height={48} className="inline-block shrink-0 rounded-xl object-cover" />
+                                <Image src={project.image} alt={`${project.title} Logo`} width={48} height={48} className="inline-block outline-1 outline-gray-950/5 shrink-0 rounded-xl object-cover" />
                                 <div className="grow">
                                     <h3 className="text-gray-950 text-sm/6 font-medium">{project.title}</h3>
                                     <p className="line-clamp-1 text-sm/6 text-neutral-600">{project.description}</p>
                                 </div>
                             </div>
-                            <div className="shrink-0 text-gray-500">
+                            <div className="shrink-0 text-gray-400">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                                     <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25" />
                                 </svg>
