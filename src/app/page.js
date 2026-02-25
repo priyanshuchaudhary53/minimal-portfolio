fa9901010d16f14f3b69daaa91b1819a984bd289
Priyanshu Chaudhary <priyanshuchaudhary53@gmail.com>
2026-02-25 05:30:24 +0530

    update product section id

--- src/app/page.js
+++ src/app/page.js
@@ -57,7 +57,7 @@ export default function Home() {
       </SectionWrapper>
 
       <SectionWrapper
-        id="side-projects"
+        id="products"
         title="Products"
         description="Below are some products I've built"
       >
