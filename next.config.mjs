0544d22939f3bf8ac23138e98608efc84a77fb82
Priyanshu Chaudhary <priyanshuchaudhary53@gmail.com>
2026-02-27 04:23:32 +0530

    add portolio redirect

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/portfolio/:slug",
+        destination: "/work",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
