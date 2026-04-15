e579986fcf3ce2c1a2e4b1a9f939c6c288d2c973
Priyanshu Chaudhary <priyanshuchaudhary53@gmail.com>
2026-04-15 16:18:34 +0530

    add FlowForms project details and logo

--- src/data/projectData.js
+++ src/data/projectData.js
@@ -1,13 +1,20 @@
 const projectData = [
     {
         id: 1,
+        title: 'FlowForms',
+        description: 'Typeform-style form builder for WordPress',
+        image: '/project/flowforms-logo.webp',
+        url: 'https://www.wpflowforms.com/',
+    },
+    {
+        id: 2,
         title: 'Htmlwind',
-        description: 'SaaS built from scratch',
+        description: 'Open-source Tailwind CSS components',
         image: '/project/htmlwind-logo.webp',
         url: 'https://htmlwind.com/',
     },
     {
-        id: 2,
+        id: 3,
         title: 'Simple Pricing Table for Elementor',
         description: 'Open-source WordPress plugin',
         image: '/project/spt-logo.gif',
